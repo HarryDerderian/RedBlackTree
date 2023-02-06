5de8c7c76e91952620800447c0df59d1485e7969
Harry Derderian <82200862+HarryDerderian@users.noreply.github.com>
2023-02-05 20:34:05 -0800

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -5,37 +5,15 @@ using namespace std;
 
 int main()
 {
+	RedBlackTree<int> tree;
+	int nums[] = {4,7,12,15,3,5,14,18,16,17};
+	//int nums[] = { 56, 12, 35, 50,39,75};
+	for (int& i : nums)
+	{
+		tree.add(i);
+		cout << i << endl;
+	}
+
+	cout << endl;
 	
-	RedBlackTree<string> tree;
-	string strings[5] = { "HARRY", "ZAC", "ADRIAN", "JEESE", "JASMINE" };
-	for (string& s : strings)
-	{
-		tree.add(s);
-	}
-	string name = "TOM";
-	string name2 = "TIM";
-	tree.add(name);
-	tree.add(name2);
-	cout << tree.size() << endl;
-	vector<string>* vec = tree.inOrder();
-	for (auto iter = vec->begin(); iter != vec->end(); iter++)
-	{
-		cout << *iter << endl;
-	}
-	
-
-	cout << "\nintegers\n\n";
-	RedBlackTree<int> tree2;
-	int nums[5] = {132,324,523,5,32487};
-	for (int& s : nums)
-	{
-		tree2.add(s);
-	}
-
-	vector<int>* vec2 = tree2.inOrder();
-	for (auto iter = vec2->begin(); iter != vec2->end(); iter++)
-	{
-		cout << *iter << endl;
-	}
-
 }
